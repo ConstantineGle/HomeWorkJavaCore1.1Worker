4cc641495a764dc6dba6a39ba8b72f89e6280a73
glebov <constantine.glebov@gmail.com>
2022-12-16 14:12:30 +0700

    Description and condition of the assignment

---
+++ src/Worker.java
@@ -0,0 +1,34 @@
+public class Worker {
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskErrorListener errorCallback) {
+        this.errorCallback = errorCallback;
+    }
+
+    public Worker(OnTaskDoneListener callback) {
+        this.callback = callback;
+    }
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String errorResult);
+    }
+}
